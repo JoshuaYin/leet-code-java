55dd89d64412d88f4394dce8b3f1962ad16ea84b
Joshua Yin <joshua.yin@icloud.com>
2020-02-19 15:16:49 +0800

    第三题最终解

--- src/main/java/im/joshua/leetcode/topic/LeetCode_3.java
+++ src/main/java/im/joshua/leetcode/topic/LeetCode_3.java
@@ -2,6 +2,9 @@ package im.joshua.leetcode.topic;
 
 import im.joshua.leetcode.util.ListNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode_3 extends LeetCodeTopic {
 /*
     给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -30,7 +33,7 @@ public class LeetCode_3 extends LeetCodeTopic {
 */
 
     public static void main(String[] args) {
-        LeetCodeTopic t3 = new LeetCode_3("dvdf");
+        LeetCodeTopic t3 = new LeetCode_3("dvddfcv");
         System.out.println(t3.solve());
     }
 
@@ -47,7 +50,20 @@ public class LeetCode_3 extends LeetCodeTopic {
 
     @Override
     public String solution() {
-        return MSG_NO_SOLUTION;
+        int res = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0, j = 0, len = string.length(); j < len; j++) {
+            char c = string.charAt(j);
+            System.out.println(String.format("Before->[c]:%s [i]:%d [j]:%d [res]:%d", c, i, j, res));
+            if (map.containsKey(c)) {
+                i = Math.max(i, map.get(c));
+            }
+            System.out.println("j - i + 1 = " + (j - i + 1));
+            res = Math.max(res, j - i + 1);
+            map.put(c, j + 1);
+            System.out.println(String.format("After->[c]:%s [i]:%d [j]:%d [res]:%d", c, i, j, res));
+        }
+        return String.valueOf(res);
     }
 
     public static ListNode<Character> parse(String str) {
